7828f63faef3c40ca0f55950520a0b3e8ad4643f
Rafael Lima <rafa.olv.lima@gmail.com>
2020-04-10 16:10:17 +0100

    first commit.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+/**
+* this main file is written just for "trying out" purposes.
+*/
+int main(int argc, char ** argv)
+{
+	std::cout << "This is example of the pythonic list lib" << std::endl;
+}
